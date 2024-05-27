90db8d7d953d7655d21b97b57ed0a4ff764bba20
Alba Nebot López <168462451+albanebotlopez@users.noreply.github.com>
2024-05-27 13:08:27 +0000

    completado

--- src/app.js
+++ src/app.js
@@ -8,4 +8,37 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  const getRandomElement = ListElement => {
+    let element = " ";
+    element = ListElement[Math.floor(Math.random() * ListElement.length)];
+    return element;
+  };
+  console.log(
+    getRandomElement(who) +
+      " " +
+      getRandomElement(action) +
+      " " +
+      getRandomElement(what) +
+      " " +
+      getRandomElement(when)
+  );
+  let excuseGen = document.querySelector("#excuse");
+  excuseGen.innerHTML =
+    getRandomElement(who) +
+    " " +
+    getRandomElement(action) +
+    " " +
+    getRandomElement(what) +
+    " " +
+    getRandomElement(when);
 };
